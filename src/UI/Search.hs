d38415dc4f2d901c53fd4a94fd15437e8590a34b
Hyperreal Gopher <hyperreal.gopher@protonmail.ch>
2020-03-10 06:07:20 -0700

    Search: hlint, haskell-stylish, and docs

--- src/UI/Search.hs
+++ src/UI/Search.hs
@@ -1,54 +1,58 @@
-module UI.Search where
+-- | Search mode for the UI. Index search implementation for UI.
+--
+-- Handle drawing the search UI, modifying the application state
+-- for the search UI, and handling Brick events for search.
+module UI.Search (searchInputUI, searchEventHandler) where
 
-import qualified Brick.Types as T
-import Brick.Widgets.Edit as E
-import Graphics.Vty.Input.Events (Event)
-import qualified Graphics.Vty as V
-import qualified Brick.Main as M
-import Control.Monad.IO.Class
+import qualified Brick.Main                as M
+import qualified Brick.Types               as T
+import           Brick.Widgets.Edit        as E
+import           Control.Monad.IO.Class
+import qualified Graphics.Vty              as V
+import           Graphics.Vty.Input.Events (Event)
 
-import UI.Util
-import GopherClient (searchGet, makeGopherMenu)
-import UI.History
-import UI.InputDialog
+import           GopherClient              (makeGopherMenu, searchGet)
+import           UI.History
+import           UI.InputDialog
+import           UI.Util
 
+-- | Draw the search prompt. Used by UI.drawUI if the gbsRenderMode
+-- is SearchMode.
 searchInputUI :: GopherBrowserState -> [T.Widget MyName]
 searchInputUI gbs = inputDialogUI editorBuffer labelText helpText
   where
     searchBuffer = gbsBuffer gbs
     editorBuffer = sbEditorState (gbsBuffer gbs)
-    labelText = "Search: " ++ (sbHost searchBuffer)
+    labelText = "Search: " ++ sbHost searchBuffer
     helpText = "Press ENTER to search"
 
--- XXX: how will location be done? FIXME this is broke currently...
+-- | Form a new application state based on a Gopher search request.
 mkSearchResponseState :: GopherBrowserState -> IO GopherBrowserState
 mkSearchResponseState gbs = do
-  let host = (sbHost $ gbsBuffer gbs)
-      port = (sbPort $ gbsBuffer gbs)
-      resource = (sbSelector $ gbsBuffer gbs)
+  let host = sbHost $ gbsBuffer gbs
+      port = sbPort $ gbsBuffer gbs
+      resource = sbSelector $ gbsBuffer gbs
       query = unlines (E.getEditContents $ sbEditorState $ gbsBuffer gbs)
   (o, selector) <- searchGet host (show port) resource query
   let newMenu = makeGopherMenu o
       location = (host, port, selector, MenuMode)
   pure $ newStateForMenu newMenu location (newChangeHistory gbs location)
-  -- XXX finish
 
---editorEventHandler :: GopherBrowserState -> Event -> T.EventM MyName Buffer
-editorEventHandler :: GopherBrowserState -> Event -> T.EventM MyName GopherBrowserState
-editorEventHandler gbs e = updateEditorInBuffer <$> E.handleEditorEvent e (sbEditorState $ gbsBuffer gbs)
-  where
-    updateEditorInBuffer x = gbs { gbsBuffer = (gbsBuffer gbs) { sbEditorState = x } }
-
+-- | The Brick application event handler for search mode. See: UI.appEvent and
+--- Brick.Main.appHandleEvent.
 searchEventHandler :: GopherBrowserState -> Event -> T.EventM MyName (T.Next GopherBrowserState)
 searchEventHandler gbs e =
   case e of
-    V.EvKey V.KEsc [] -> M.continue $ returnSearchFormerState gbs
-    -- FIXME: needs to make search request
+    -- FIXME: esc quits! Change key...
+    V.EvKey V.KEsc []   -> M.continue $ returnSearchFormerState gbs
     V.EvKey V.KEnter [] -> liftIO (mkSearchResponseState gbs) >>= M.continue
-    _ -> M.continue =<< editorEventHandler gbs e
-    --V.EvKey V.KBS [] -> M.continue $ updateQuery $ take (length curQuery - 1) curQuery
-    --V.EvKey (V.KChar c) [] ->
-    --  M.continue $ gbs { gbsBuffer = (gbsBuffer gbs) { sbQuery = curQuery ++ [c] } }
-    --_ -> M.continue gbs
+    _                   -> M.continue =<< editorEventHandler gbs e
   where
-    returnSearchFormerState g = g {gbsBuffer = (sbFormerBufferState $ gbsBuffer g), gbsRenderMode = MenuMode}
+    returnSearchFormerState g = g {gbsBuffer = sbFormerBufferState $ gbsBuffer g, gbsRenderMode = MenuMode}
+
+    -- | A modification of the default Brick.Widgets.Edit event handler; changed to
+    -- return a GopherBrowserState instead of just an editor state.
+    editorEventHandler :: GopherBrowserState -> Event -> T.EventM MyName GopherBrowserState
+    editorEventHandler gbs e =
+      let updateEditorInBuffer x = gbs { gbsBuffer = (gbsBuffer gbs) { sbEditorState = x } }
+      in updateEditorInBuffer <$> E.handleEditorEvent e (sbEditorState $ gbsBuffer gbs)
