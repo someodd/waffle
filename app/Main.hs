8577f112a8239bee4fa01c9bfaa78426351dbb41
someodd <someodd@pm.me>
2020-05-02 23:56:26 -0700

    Fix invalid starting args errors/pattern match

--- app/Main.hs
+++ app/Main.hs
@@ -5,12 +5,12 @@ import System.Environment
 
 import UI
 
+handleArgs :: [String] -> IO ()
+handleArgs []                      = uiMain Nothing
+handleArgs (host:port:resource:[]) = uiMain (Just (T.pack host, read port :: Int, T.pack resource))
+handleArgs (_)                     = error "Error! Need to supply host, port, and selector (or no args!). For empty selector you can use \"\"."
+
 main :: IO ()
 main = do
   args <- getArgs
-  if null args then
-    uiMain Nothing
-  else
-    -- FIXME: what if pattern exhaustion?
-    let [host, port, resource] = args
-    in  uiMain (Just (T.pack host, read port :: Int, T.pack resource))
+  handleArgs args
