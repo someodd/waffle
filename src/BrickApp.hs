e19fb867291a43f6a11671ec7db2c5e74702612d
Hyperreal Gopher <hyperrealgopher@protonmail.ch>
2020-09-16 09:51:15 -0700

    WIP: Implement homepage; mk openconfig use visible
    Replace openconfig mdoe's viewport scrolling
    system simply by making the render editor use
    the `visible` wrapper.
    
    Implement the homepage system, but not the
    homepage setter in TUI.

--- src/BrickApp.hs
+++ src/BrickApp.hs
@@ -18,13 +18,13 @@ import qualified Brick.Main                    as B
 import qualified Graphics.Vty                  as V
 import           Brick.Widgets.Core             ( txt )
 
+import Homepage
 import BrickApp.Types
 import BrickApp.Types.Names
 import BrickApp.Utils
 import BrickApp.Draw
 import BrickApp.Handle
 import BrickApp.ModeAction.Progress
-import BrickApp.ModeAction.Help
 import BrickApp.Utils.Style
 
 theApp :: B.App GopherBrowserState CustomEvent AnyName
@@ -35,6 +35,7 @@ theApp = B.App { B.appDraw         = drawUI
                , B.appAttrMap      = const theMap
                }
 
+-- FIXME: interpret homepage from config
 -- FIXME: isn't there a way to infer a location's type? Assuming first
 -- link is a menu is a horrible hack...
 --
@@ -59,10 +60,11 @@ uiMain possibleLocation = do
     , gbsStatus = Nothing
     }
 
-  initialState <- if null possibleLocation then
+  initialState <- if null possibleLocation then do
       -- if we didn't get a location passed to us, then we want to
-      -- start with the help page!
-      modifyGbsForHelp dummyStateToOverride
+      -- start with the home page from the config!
+      goHome dummyStateToOverride
+      -- modifyGbsForHelp dummyStateToOverride
     else
       -- ...otherwise let's open the page supplied!
       let (host, port, magicString) = fromJust possibleLocation
